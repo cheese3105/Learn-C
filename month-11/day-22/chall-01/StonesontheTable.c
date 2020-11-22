5942d9f801c07d974ebb48501b4dd5fae0336eb1
cheese3105 <74854445+cheese3105@users.noreply.github.com>
2020-11-22 20:46:18 +0700

    Update StonesontheTable.c

--- month-11/day-22/chall-01/StonesontheTable.c
+++ month-11/day-22/chall-01/StonesontheTable.c
@@ -1,12 +1,12 @@
 unsigned solution(const char *stones) {
-  int sol = 0;
-  char a;
-  a = stones[0];
-  for(int i = 1; i<strlen(stones); i++)
+  int sol = 0; //sol là số ký tự cần phải bỏ ra
+  char a; 
+  a = stones[0]; //gán ký tự đầu tiên của chuỗi vào a
+  for(int i = 1; i<strlen(stones); i++) //dùng for để duyệt qua từng ký tự trong chuỗi
     {
-      if (a == stones[i])
-        {  sol++;  }
-      a = stones[i];
-    }
+      if (a == stones[i]) //nếu ký tự sau giống với ký tự trước đó
+        {  sol++;  }//thì sol + 1 tức là bỏ đi ký tự đằng trước
+      a = stones[i];//thay a bằng ký tự đằng sau
+    }//tiếp tục thực hiện đến hết chuỗi
   return sol;          
 }
