d0bafd167e32985014dd27f63fbed0c53d9681a1
cheese3105 <74854445+cheese3105@users.noreply.github.com>
2020-11-22 21:34:02 +0700

    Update StonesontheTable.c

--- month-11/day-22/chall-01/StonesontheTable.c
+++ month-11/day-22/chall-01/StonesontheTable.c
@@ -1,4 +1,5 @@
-/*There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
+/*
+There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B.
 
 Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colours.
 
@@ -6,7 +7,10 @@ Examples:
 
 "RGBRGBRGGB"   => 1
 "RGGRGBBRGRR"  => 3
-"RRRRGGGGBBBB" => 9*/
+"RRRRGGGGBBBB" => 9
+
+link: https://www.codewars.com/kata/5f70e4cce10f9e0001c8995a/train/c
+*/
 
 unsigned solution(const char *stones) {
   int sol = 0; //sol là số ký tự cần phải bỏ ra
